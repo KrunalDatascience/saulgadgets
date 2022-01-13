4054da8db70416150577105475611f15afa19347
KrunalDatascience <krunal.datascience@gmail.com>
2022-01-13 19:41:22 +0530

    Adding the new Model named Product. Setting foregin key from the Category model. Registering the DB model in admin view. Making Migrations.

--- apps/store/models.py
+++ apps/store/models.py
@@ -1,5 +1,7 @@
+from distutils.text_file import TextFile
 from tabnanny import verbose
 from turtle import title
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
@@ -12,4 +14,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
-    
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.title
