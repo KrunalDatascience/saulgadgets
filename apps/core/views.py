a88ece9bab0db9d9815e6909d50fc2e1de7eeb05
KrunalDatascience <krunal.datascience@gmail.com>
2022-01-13 20:31:49 +0530

    Get list of Products and show it on the fronpage

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,8 +1,17 @@
+from multiprocessing import context
 from django.shortcuts import render
+
+from apps.store.models import Product
 
 # Create your views here.
 def frontpage(request):
-    return render(request, 'frontpage.html')
+    products = Product.objects.all()
+
+    context = {
+        'products' : products
+    }
+
+    return render(request, 'frontpage.html', context)
 
 def contactpage(request):
     return render(request, 'contact.html')
