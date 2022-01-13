ef7ca4f0de0b49103bcda871eee57dc8f126a109
KrunalDatascience <krunal.datascience@gmail.com>
2022-01-13 23:48:34 +0530

    Creating detailed product vview: - Frontpage of the core app calls the product_detail URLConfig and passes product.slug variable. - This slug variable is captured in the URLConfig and passed as URL pattern. - This url pattern (slug) is captured by the product_detailed view as a parameter and used as an object filter in get_object_or_404() to filter the product in DB associated with the passed slug. Which is further used in the detailed view page

--- apps/store/views.py
+++ apps/store/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from itertools import product
+from multiprocessing import context
+from django.shortcuts import render, get_object_or_404
 
+from .models import Product
 # Create your views here.
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug = slug)
+    context = {
+        'product': product
+    }
+    return render(request, 'product-details.html', context)
--- saulgadgets/urls.py
+++ saulgadgets/urls.py
@@ -13,15 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from inspect import ismethoddescriptor
 from django.contrib import admin
 from django.urls import path
 
 ### apps urls views
-from apps.core import views 
+from apps.core.views import frontpage, contactpage 
+from apps.store.views import product_detail
 
 urlpatterns = [
-    path('', views.frontpage, name = 'frontpage'),
-    path('contact/', views.contactpage, name = 'contactpage'),
+    # App: core
+    path('', frontpage, name = 'frontpage'),
+    path('contact/', contactpage, name = 'contactpage'),
+
+    #App: Store
+    path('<slug:slug>/', product_detail, name = 'product_detail'),
 
     path('admin/', admin.site.urls),
 ]
